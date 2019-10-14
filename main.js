10c6bad46c008ed959057dae6ba4e1689fec153f
Brook West <bmwest42@gmail..com>
2019-10-14 15:48:30 -0700

    Extension 6

--- main.js
+++ main.js
@@ -10,3 +10,34 @@ document.addEventListener("DOMContentLoaded", function() {
       });
     });
   });
+
+  $( document ).ready(function() {
+    $( "#button" ).click(function() {
+      $( "#fade" ).fadeOut( "slow", function() {
+      });
+    });
+  });
+  $( document ).ready(function() {
+    $( "#button" ).click(function() {
+      $( "#fade2" ).fadeOut( "slow", function() {
+      });
+    });
+  });
+  $( document ).ready(function() {
+    $( "#button" ).click(function() {
+      $( "#fade3" ).fadeOut( "slow", function() {
+      });
+    });
+  });
+  $( document ).ready(function() {
+    $( "#button" ).click(function() {
+      $( "#fade4" ).fadeOut( "slow", function() {
+      });
+    });
+  });
+  $( document ).ready(function() {
+    $( "#button" ).click(function() {
+      $( "#fade5" ).fadeOut( "slow", function() {
+      });
+    });
+  });
